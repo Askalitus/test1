0399dc505c4aad1aaebc208f8df5352bb50dba47
Askalitus <popugaq6@mail.ru>
2022-02-28 10:15:40 +0300

    И ещё

---
+++ Столетия/Столетия/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lolita
+{
+    class Goda
+    {
+        static void Main()
+        {
+
+            Console.Write("Введите год: ");
+
+            int god = int.Parse(Console.ReadLine());
+
+            Console.Write("В этом году: ");
+
+            if (god % 100 == 0)
+            {
+                Console.Write("{0} Столетие", god / 100);
+
+            }
+            else
+            {
+                Console.Write("{0} Столетие", god / 100 + 1);
+            }
+        }
+    }
+}
