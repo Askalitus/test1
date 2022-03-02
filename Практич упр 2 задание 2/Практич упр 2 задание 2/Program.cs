586b09912511aaad27d95a050adec1e2979f523d
Askalitus <popugaq6@mail.ru>
2022-03-02 14:19:12 +0300

    это надо доделать

---
+++ Практич упр 2 задание 2/Практич упр 2 задание 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace lolita
+{
+    class Ex2
+    {
+        static void Main()
+        {
+            Console.WriteLine("k= ");
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("m= ");
+            int m = int.Parse(Console.ReadLine());
+        }
+    }
+}
