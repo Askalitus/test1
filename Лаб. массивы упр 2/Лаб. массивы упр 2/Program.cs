a6a862a1bfd8f6366e4c39b3fa252788c5af12e2
Askalitus <popugaq6@mail.ru>
2022-03-10 09:28:21 +0300

    лаба массивы

--- Лаб. массивы упр 2/Лаб. массивы упр 2/Program.cs
+++ Лаб. массивы упр 2/Лаб. массивы упр 2/Program.cs
@@ -5,7 +5,53 @@ namespace lolita
     {
         static void Main()
         {
+            int[,] a = Input();
 
+            int[,] b = Input();
+            int[,] result = Multiply(a, b);
+            Output(result);
+
+        }
+
+        private static int[,] Input()
+        {
+            int[,] a = new int[2, 2];
+            for (int r = 0; r < a.GetLength(0); r++)
+            {
+                for (int c = 0; c < a.GetLength(1); c++)
+                {
+                    Console.Write("Enter value for [ {0}, {1} ] : ", r, c);
+                    string s = System.Console.ReadLine();
+                    a[r,c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+            return a;
+        }
+
+        private static int[,] Multiply(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    result[r, c] += a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+            return result;
+        }
+
+        private static void Output(int[,] result)
+        {
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for(int c = 0; c < result.GetLength(1); c++)
+                {
+                    Console.Write("{0} ", result[r, c]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
